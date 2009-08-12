e1f32df065ad4fce161cd51e1fe9320a40599156
yllan <yllan@me.com>
2009-08-13 04:14:27 +0800

    Set a pointer to nil after releasing it. (It doesn't hurt, but useful when you do UIViewController cleanup.) Use (nonatomic, copy) for immutable collection classes. (NSString|NSSet|NSArray|NSDictionary)
    Signed-off-by: Mike Schrag <mschrag@mdimension.com>

--- PropertyFromInstanceVariable.pl
+++ PropertyFromInstanceVariable.pl
@@ -1,5 +1,5 @@
 #! /usr/bin/perl -w
-
+ 
 #  Created by Matt Gallagher on 20/10/08.
 #  Copyright 2008 Matt Gallagher. All rights reserved.
 #
@@ -8,29 +8,29 @@
 #  that any redistribution (in part or whole) of source code must retain
 #  this copyright and permission notice. Attribution in compiled projects is
 #  appreciated but not required.
-
+ 
 use strict;
-
+ 
 # Get the header file contents from Xcode user scripts
 my $headerFileContents = <<'HEADERFILECONTENTS';
 %%%{PBXAllText}%%%
 HEADERFILECONTENTS
-
+ 
 # Get the indices of the selection from Xcode user scripts
 my $selectionStartIndex = %%%{PBXSelectionStart}%%%;
 my $selectionEndIndex = %%%{PBXSelectionEnd}%%%;
-
+ 
 # Get path of the header file
 my $implementationFilePath = "%%%{PBXFilePath}%%%";
 my $headerFilePath = $implementationFilePath;
-
+ 
 # Look for an implemenation file with a ".m" or ".mm" extension
 $implementationFilePath =~ s/\.[hm]*$/.m/;
 if (!(-e $implementationFilePath))
 {
 	$implementationFilePath =~ s/.m$/.mm/;
 }
-
+ 
 # Handle subroutine to trime whitespace off both ends of a string
 sub trim
 {
@@ -42,14 +42,14 @@ sub trim
 # Get the selection out of the header file
 my $selectedText =  substr $headerFileContents, $selectionStartIndex, ($selectionEndIndex - $selectionStartIndex);
 $selectedText = trim $selectedText;
-
+ 
 my $type = "";
 my $asterisk = "";
 my $name = "";
 my $ivarName = "";
 my $behavior = "";
 my $isPointer = 0;
-
+ 
 # Test that the selection is:
 #  At series of identifiers (the type name and access specifiers)
 #  Possibly an asterisk
@@ -72,7 +72,13 @@ if (length($selectedText) && ($selectedText =~ /([_A-Za-z][_A-Za-z0-9]*\s*)+([\s
 	if (defined($asterisk) && length($asterisk) == 1)
 	{
 		$isPointer = 1;
-		$behavior = "(nonatomic, retain) ";
+        if ($type eq "NSArray" || $type eq "NSString" || $type eq "NSDictionary" || $type eq "NSSet") {
+            $behavior = "(nonatomic, copy) ";
+        } 
+        else 
+        {
+            $behavior = "(nonatomic, retain) ";
+        }
 	}
 	else
 	{
@@ -85,7 +91,7 @@ else
 {
 	exit 1;
 }
-
+ 
 # Find the closing brace (end of the class variables section)
 my $remainderOfHeader = substr $headerFileContents, $selectionEndIndex;
 my $indexAfterClosingBrace = $selectionEndIndex + index($remainderOfHeader, "\n}\n") + 3;
@@ -93,7 +99,7 @@ if ($indexAfterClosingBrace == -1)
 {
 	exit 1;
 }
-
+ 
 # Determine if we need to add a newline in front of the property declaration
 my $leadingNewline = "\n";
 if (substr($headerFileContents, $indexAfterClosingBrace, 1) eq "\n")
@@ -101,18 +107,18 @@ if (substr($headerFileContents, $indexAfterClosingBrace, 1) eq "\n")
 	$indexAfterClosingBrace += 1;
 	$leadingNewline = "";
 }
-
+ 
 # Determine if we need to add a newline after the property declaration
 my $trailingNewline = "\n";
 if (substr($headerFileContents, $indexAfterClosingBrace, 9) eq "\@property")
 {
 	$trailingNewline = "";
 }
-
+ 
 # Create and insert the propert declaration
 my $propertyDeclaration = $leadingNewline . "\@property " . $behavior . $type . " " . $asterisk . $name . ";\n" . $trailingNewline;
 substr($headerFileContents, $indexAfterClosingBrace, 0) = $propertyDeclaration;
-
+ 
 my $replaceFileContentsScript = <<'REPLACEFILESCRIPT';
 on run argv
 	set fileAlias to POSIX file (item 1 of argv)
@@ -123,19 +129,17 @@ on run argv
 	end tell
 end run
 REPLACEFILESCRIPT
-
-$headerFileContents =~ s#\s*$##;
-
+ 
 # Use Applescript to replace the contents of the header file
 # (I could have used the "Output" of the Xcode user script instead)
 system 'osascript', '-e', $replaceFileContentsScript, $headerFilePath, $headerFileContents;
-
+ 
 # Stop now if the implementation file can't be found
 if (!(-e $implementationFilePath))
 {
 	exit 1;
 }
-
+ 
 my $getFileContentsScript = <<'GETFILESCRIPT';
 on run argv
 	set fileAlias to POSIX file (item 1 of argv)
@@ -146,18 +150,18 @@ on run argv
 	return docText
 end run
 GETFILESCRIPT
-
+ 
 # Get the contents of the implmentation file
 open(SCRIPTFILE, '-|') || exec 'osascript', '-e', $getFileContentsScript, $implementationFilePath;
 my $implementationFileContents = do {local $/; <SCRIPTFILE>};
 close(SCRIPTFILE);
-
+ 
 # Look for the class implementation statement
 if (length($implementationFileContents) && ($implementationFileContents =~ /(\@implementation [_A-Za-z][_A-Za-z0-9]*\n)/))
 {
 	my $matchString = $1;
 	my $indexAfterMatch = index($implementationFileContents, $matchString) + length($matchString);
-
+ 
 	# Determine if we want a newline before the synthesize statement
 	$leadingNewline = "\n";
 	if (substr($implementationFileContents, $indexAfterMatch, 1) eq "\n")
@@ -165,14 +169,14 @@ if (length($implementationFileContents) && ($implementationFileContents =~ /(\@i
 		$indexAfterMatch += 1;
 		$leadingNewline = "";
 	}
-
+ 
 	# Determine if we want a newline after the synthesize statement
 	$trailingNewline = "\n";
 	if (substr($implementationFileContents, $indexAfterMatch, 11) eq "\@synthesize")
 	{
 		$trailingNewline = "";
 	}
-
+ 
 	# Create and insert the synthesize statement 
 	my $synthesizeStatement;
 	if ($ivarName ne $name) {
@@ -182,17 +186,15 @@ if (length($implementationFileContents) && ($implementationFileContents =~ /(\@i
 		$synthesizeStatement = $leadingNewline . "\@synthesize " . $name . ";\n" . $trailingNewline;
 	}
 	substr($implementationFileContents, $indexAfterMatch, 0) = $synthesizeStatement;
-
+ 
 	if ($isPointer) {
-		if ($implementationFileContents !~ s#(\(void\)\s*dealloc\s*\{\s*\n)(\s*)#$1$2\[$ivarName release\];\n$2#s) {
-        		$implementationFileContents =~ s#(\@end)#\n- (void)dealloc {\n\t[$ivarName release];\n\t[super dealloc];\n}\n$1#s;
+		if ($implementationFileContents !~ s#(\(void\)\s*dealloc\s*\{\s*\n)(\s*)#$1$2\[$ivarName release\], $ivarName = nil;\n$2#s) {
+        		$implementationFileContents =~ s#(\@end)#\n- (void)dealloc {\n\t[$ivarName release], $ivarName = nil;\n\t[super dealloc];\n}\n$1#s;
 		}
 	}
-
-	$implementationFileContents =~ s#\s*$##;
-
+ 
 	# Use Applescript to replace the contents of the implementation file in Xcode
 	system 'osascript', '-e', $replaceFileContentsScript, $implementationFilePath, $implementationFileContents;
 }
-
+ 
 exit 0;
